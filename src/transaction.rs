47ab2c31cf42f7f279e8afd865a7adf29bdbd45c
masami87 <chitanda2012@outlook.com>
2023-04-17 22:50:14 +0800

    Add error and tx.

---
+++ src/transaction.rs
@@ -0,0 +1,39 @@
+use crate::script::Script;
+
+pub struct TxIn {
+    /// Prev transaction ID: hash256 of prev tx contents.
+    prev_tx: Vec<u8>,
+    /// UTXO output index in the transaction.
+    prev_index: u64,
+    /// Unlocking script.
+    script_sig: Script,
+    /// Almost not use today.
+    sequence: u32,
+}
+
+impl Default for TxIn {
+    fn default() -> Self {
+        Self {
+            prev_tx: Vec::new(),
+            prev_index: 0,
+            script_sig: Script,
+            sequence: 0,
+        }
+    }
+}
+
+pub struct TxOut {
+    /// In units of satoshi(1e-8 of a bitcoin)
+    amount: u64,
+    /// Locking script.
+    script_pubkey: Script,
+}
+
+impl Default for TxOut {
+    fn default() -> Self {
+        Self {
+            amount: 0,
+            script_pubkey: Script,
+        }
+    }
+}
